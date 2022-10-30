e175b3773b66b0603d0dd075925e42b9b5e6ec47
Daniel Gómez <88523667+Danigom11@users.noreply.github.com>
2022-10-30 14:53:31 +0100

    Java

---
+++ src/com/company/etiposdatosavanzados/ArraysLists.java
@@ -0,0 +1,13 @@
+package com.company.etiposdatosavanzados;
+
+import java.util.ArrayList;
+
+public class ArraysLists {
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<String>();
+        lista.add("ElementoA");
+        lista.add("ElementoB");
+        lista.add("ElementoC");
+
+    }
+}
---
+++ src/com/company/etiposdatosavanzados/Strings.java
@@ -0,0 +1,48 @@
+package com.company.etiposdatosavanzados;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String cadena = "Mensaje de texto";
+        System.out.println("La cadena de texto es: " + cadena);
+
+        int cadenaLen = cadena.length();
+        System.out.println("La longitud de la cadena es de: " + cadenaLen);
+
+        String cadenaMin = cadena.toLowerCase();
+        System.out.println("La cadena en minúsculas: " + cadenaMin);
+
+        String cadenaMax = cadena.toUpperCase();
+        System.out.println("La cadena en mayúsculas: " + cadenaMax);
+
+        // Empieza por
+        boolean resultadoEmpiezaPor = cadena.startsWith("men");
+        if (resultadoEmpiezaPor) {
+            System.out.println("La cadena empieza por lo que estoy buscando");
+        } else {
+            System.out.println("La cadena no empieza por lo que quiero");
+        }
+
+        // Termina en
+        boolean resultadoTerminaPor = cadena.endsWith("o");
+        if (resultadoTerminaPor) {
+            System.out.println("La cadena termina por lo que estoy buscando");
+        } else {
+            System.out.println("La cadena no termina por lo que quiero");
+        }
+
+        // Caracter en
+        char letra = cadena.charAt(4);
+        System.out.println("Caracter es: " + letra);
+
+        // Imprimir caracteres uno por uno
+        for (int i = 0; i < cadena.length(); i++) {
+            System.out.println("Carácter actual: " + cadena.charAt(i));
+        }
+
+        // Caracteres al revés
+        for (int i = cadena.length()-1; i >= 0; i--) {
+            System.out.println("Cadena al revés: " + cadena.charAt(i));
+        }
+    }
+}
